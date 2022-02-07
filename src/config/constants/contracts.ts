dada79857d5a4f310889dc48d91826d394fb8fcc
Crunqly03 <safaozince@gmail.com>
2022-02-07 16:04:22 +0300

    initialize

---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6', // Goose Golden Egg (EGG)
+    97: '0xe90685e7Ca8f7a61ea5086965A1242919061E519', // Wisteria Swap Token (WST)
+  },
+  masterChef: {
+    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
+    97: '0x5C428CA6eD278093064eb7953a5Bf3B441a3430A',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7',
+  },
+}
